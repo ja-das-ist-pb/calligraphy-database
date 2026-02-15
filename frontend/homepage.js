7f6748b38db3084a43503182222607607f5cb77e
Henry960917 <148993233+Henry960917@users.noreply.github.com>
2026-02-15 23:12:55 +0800

    word123 finished but untested and added a cute turtle

--- frontend/homepage.js
+++ frontend/homepage.js
@@ -10,121 +10,134 @@ const initialPage = document.getElementById("initial-page");
 const afterSearch = document.getElementById("after-search");
 const closeUp = document.getElementById("close-up");
 
+//word1,2,3...
+const word123 = document.getElementById("word123");
+
 //查詢的字轉陣列
-function getChar() {
-  let arr = [];
-  const word = searchBox.value.trim();
-  if (word !== "") {
-    arr.push(word);
-  }
-  return arr;
+function getChar(){
+    let arr=[];
+    const word = searchBox.value.trim();
+    if(word!==""){
+        arr.push(word);
+    }
+    return arr;
 }
 
 //回傳checkbox被勾的value
-function getChecked(checkboxes) {
-  let arr1 = [];
-  for (let i = 0; i < checkboxes.length; i++) {
-    if (checkboxes[i].checked) {
-      arr1.push(checkboxes[i].value);
+function getChecked(checkboxes){
+    let arr1=[];
+    for(let i = 0; i<checkboxes.length; i++){
+        if(checkboxes[i].checked){
+            arr1.push(checkboxes[i].value);
+        }
     }
-  }
-  return arr1;
+    return arr1;
 }
 
-function collectData() {
-  return {
-    type: "calligraphy",
-    char: getChar(),
-    author: getChecked(authorCheckbox),
-    font: getChecked(fontCheckbox),
-  };
+function collectData(){
+    return {
+        type: "calligraphy",
+        char: getChar(),
+        author: getChecked(authorCheckbox),
+        font: getChecked(fontCheckbox)
+    };
 }
 
 //來搜吧
-function search() {
-  initialPage.style.display = "none";
-  //有輸入才開始搜尋中
-  if (searchBox.value.trim() === "") {
-    return;
-  }
-  const requestData = collectData();
-  afterSearch.innerHTML = "搜尋中，請稍後......";
-  fetch("http://127.0.0.1:8000/search-calligraphy", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json", // 告訴後端，我傳 JSON，而且我JJ很大
-    },
-    body: JSON.stringify(requestData), // JS 物件轉 JSON
-  })
-    .then(function (response) {
-      return response.json();
+function search(){
+    initialPage.style.display = "none";
+    //有輸入才開始搜尋中
+    if(searchBox.value.trim() === ""){
+        return;
+    }
+
+    //word1,2,3...
+    word123.innerHTML="";
+    const words = searchBox.value.trim();
+    const chars = words.split("");
+    for(let i = 0; i<chars.length; i++){
+        const div = document.createElement("div");
+        div.className = "word" + (i+1);
+        div.innerHTML = chars[i];
+        word123.appendChild(div);
+    }
+
+    const requestData = collectData();
+    afterSearch.innerHTML = "搜尋中，請稍後......"
+    fetch("http://127.0.0.1:8000/search-calligraphy", 
+        {
+        method: "POST", 
+        headers: {
+            "Content-Type": "application/json" // 告訴後端，我傳 JSON，而且我JJ很大
+        },
+        body: JSON.stringify(requestData)    // JS 物件轉 JSON
+        }
+    )
+    .then(function(response){
+        return response.json();
     })
-    .then(function (data) {
-      printIt(data);
+    .then(function(data){
+        printIt(data);
     })
-    .catch(function () {
-      afterSearch.innerHTML = "搜尋失敗";
+    .catch(function(){
+        afterSearch.innerHTML = "搜尋失敗";
     });
 }
 
 //顯示結果
-function printIt(data) {
-  afterSearch.innerHTML = "";
-  if (!data || data.length === 0) {
-    afterSearch.innerHTML = "無搜尋結果";
-    return;
-  }
-  for (let i = 0; i < data.length; i++) {
-    const photo = data[i];
-    const div = document.createElement("div");
-    div.className = "photo";
-    div.innerHTML =
-      "<img src='" +
-      photo.image +
-      "'>" +
-      "<p>" +
-      photo.char +
-      "｜" +
-      photo.author +
-      "｜" +
-      photo.font +
-      "</p>";
-    afterSearch.appendChild(div);
-    //放大功能
-    div.addEventListener("click", function () {
-      zoomIn(photo);
-    });
-  }
+function printIt(data){
+    afterSearch.innerHTML = "";
+    if(!data || data.length===0){
+        afterSearch.innerHTML = "無搜尋結果";
+        return;
+    }
+    const chars = searchBox.value.trim().split("");
+    for(let i = 0; i<data.length; i++){
+        const photo = data[i];
+        const div = document.createElement("div");
+        div.className = "photo";
+        div.innerHTML ="<img src='" + photo.image + "'>" + "<p>" + photo.char + "</p>" +
+        "<p>" + photo.author + "</p>" +
+        "<p>" + photo.font + "</p>";
+        afterSearch.appendChild(div);
+        //放大功能
+        div.addEventListener("click", function(){
+            zoomIn(data);
+        });
+    }
 }
 
 //放大特寫
-function zoomIn(photo) {
-  closeUp.innerHTML =
-    "<div class='close-up-bgd'></div>" +
-    "<div class='close-up-content'>" +
-    "<img src='" +
-    photo.image +
-    "'>" +
-    "<h3>" +
-    photo.char +
-    "</h3>" +
-    "<p>" +
-    photo.author +
-    "｜" +
-    photo.font +
-    "</p>" +
-    "</div>";
-  const bgd = closeUp.querySelector(".close-up-bgd");
-  bgd.addEventListener("click", closeIt);
+function zoomIn(photo){
+    closeUp.innerHTML ="<div class='close-up-bgd'></div>" +"<div class='close-up-content'>" +"<img src='" + photo.image + "'>" +"<h3>" + photo.char + "</h3>" +
+    "<p>" + photo.author + "｜" + photo.font + "</p>" + "</div>";
+    const bgd = closeUp.querySelector(".close-up-bgd");
+    bgd.addEventListener("click", closeIt);
 }
 //關
-function closeIt() {
-  closeUp.innerHTML = "";
+function closeIt(){
+    closeUp.innerHTML = "";
 }
 
 //按ENTER查
-searchBox.addEventListener("keydown", function (e) {
-  if (e.key === "Enter") {
-    search();
-  }
-});
+searchBox.addEventListener("keydown", function(e){
+    if(e.key==="Enter"){
+        search();
+    }
+})
+
+//小龜龜-偵測bug大師
+/*⣠⡶⠚⠛⠲⢄⡀
+⣼⠁      ⠀⠀⠀⠳⢤⣄
+⢿⠀⢧⡀⠀⠀⠀⠀⠀⢈⡇
+⠈⠳⣼⡙⠒⠶⠶⠖⠚⠉⠳⣄
+⠀⠀⠈⣇⠀⠀⠀⠀⠀⠀⠀⠈⠳⣄
+⠀⠀⠀⠘⣆       ⠀⠀⠀⠀⠀⠈⠓⢦⣀
+⠀⠀⠀⠀⠈⢳⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠲⢤
+⠀⠀⠀⠀⠀⠀⠙⢦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢧
+⠀⠀⠀⠀⠀⠀⠀⡴⠋⠓⠦⣤⡀⠀⠀⠀⠀⠀⠀⠀⠈⣇
+⠀⠀⠀⠀⠀⠀⣸⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡄ 
+⠀⠀⠀⠀⠀⠀⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇
+⠀⠀⠀⠀⠀⠀⢹⡄⠀⠀⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠃
+⠀⠀⠀⠀⠀⠀⠀⠙⢦⣀⣳⡀⠀⠀⠀⠀⠀⠀⠀⠀⣰⠏
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠛⢦⣀⣀⣀⣀⣠⡴⠚⠁*/
