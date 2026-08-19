90075a62a466f44521fa93905fcdd4574f5eb454
ja-das-ist-pb <paulpb0725@gmail.com>
2026-08-19 23:37:46 +0800

    fix some bugs in counter.js

--- frontend/counter.js
+++ frontend/counter.js
@@ -7,14 +7,18 @@ const month = document.getElementById("month");
 const year = document.getElementById("year");
 const total = document.getElementById("total");
 
-const updataele = [today, yesterday, week, month, year, total];
+const updateele = [today, yesterday, week, month, year, total];
 
 async function search() {
-    const response = await fetch(`${BASE_URL}/visit/status`);
-    const data = await JSON.parse(response.json());
-    updateele.forEach(function(ele){
-        ele.innerText = data.string(ele);
-    })
+    try {
+        const response = await fetch(`${BASE_URL}/visit/status`);
+        const data = await JSON.parse(response.json());
+        updateele.forEach(function(ele){
+            ele.innerText = data[ele.id] ?? "---";
+        })
+    } catch(err) {
+        console.log("造訪人數讀取失敗 : ", err)
+    }
 
     // test
     console.log(data)
